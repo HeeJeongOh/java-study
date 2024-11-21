6aaeb40f0c697a6ff8e3f46a87e1646df056ed9f
HeeJeongOh <ohhj1999@gmail.com>
2024-11-21 15:23:15 +0900

    생성자 오버로

--- chapter03/src/main/java/chapter03/Song.java
+++ chapter03/src/main/java/chapter03/Song.java
@@ -1,7 +1,7 @@
 package chapter03;
 
 public class Song {
-	
+
 	private String title;
 	private String artist;
 	private String album;
@@ -9,52 +9,79 @@ public class Song {
 	private int year;
 	private int track;
 	
+	// 오버로딩 
+	public Song(String title, String artist) {
+//		코드 중복 제거 
+//		this.title = title;
+//		this.artist = artist;		
+//		System.out.println("some code1");
+
+		// 다른 생성자 호출 
+		this(title, artist, "", "", 0, 0);
+	}
+
+	public Song(String title, String artist, String album, String composer, int year, int track) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
+		this.track = track;
+		
+		System.out.println("some code1");
+	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getArtist() {
 		return artist;
 	}
+
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
+
 	public String getAlbum() {
 		return album;
 	}
+
 	public void setAlbum(String album) {
 		this.album = album;
 	}
+
 	public String getComposer() {
 		return composer;
 	}
+
 	public void setComposer(String composer) {
 		this.composer = composer;
 	}
+
 	public int getYear() {
 		return year;
 	}
+
 	public void setYear(int year) {
 		this.year = year;
 	}
+
 	public int getTrack() {
 		return track;
 	}
+
 	public void setTrack(int track) {
 		this.track = track;
 	}
-	
 
 	public void show() {
 		System.out.println(
-				artist + " " 
-				+ title + "("
-				+ album + ", "
-				+ year + ", "
-				+ track + "번 track, "
-				+ composer + " 작곡)");
+				artist + " " + title + "(" + album + ", " + year + ", " + track + "번 track, " + composer + " 작곡)");
 	}
 
 }
--- chapter03/src/main/java/chapter03/SongTest.java
+++ chapter03/src/main/java/chapter03/SongTest.java
@@ -3,16 +3,22 @@ package chapter03;
 public class SongTest {
 
 	public static void main(String[] args) {
-		Song song = new Song();
+//		Song song = new Song();
+//		
+//		song.setTitle("좋은 날");
+//		song.setArtist("아이유");
+//		song.setAlbum("Real");
+//		song.setComposer("이민수");
+//		song.setYear(2010);
+//		song.setTrack(3);
 		
-		song.setTitle("좋은 날");
-		song.setArtist("아이유");
-		song.setAlbum("Real");
-		song.setComposer("이민수");
-		song.setYear(2010);
-		song.setTrack(3);
+		// 생성자를 통한 객체 초기화 
+		Song song1 = new Song("좋은 날","아이유", "Real", "이민수", 2010, 3);
+		song1.show();
+		
+		Song song2 = new Song("Ditto", "뉴진스");
+		song2.show();
 
-		song.show();
 	}
 
 }
