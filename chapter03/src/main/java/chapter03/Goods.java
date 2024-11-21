13c1124a2e6b8c0eff4b67d3c4f63bf90197fd51
HeeJeongOh <ohhj1999@gmail.com>
2024-11-21 13:23:20 +0900

    getter, setter

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,8 +1,51 @@
 package chapter03;
 
 public class Goods {
-	String name;
-	int price;
-	int countStock;
-	int countSold;
+
+	private String name;
+	private int price;
+	private int countSold;
+	private int countStock;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getCountSold() {
+		return countSold;
+	}
+
+	public void setCountSold(int countSold) {
+		this.countSold = countSold;
+	}
+	
+	public int getCountStock() {
+		return countStock;
+	}
+
+	public void setCountStock(int countStock) {
+		this.countStock = countStock;
+	}
+
+	public void printInfo() {
+		System.out.println(
+				// 생략 가능 
+				"상품 이름: " + name
+				+ ", 가격: " + price
+				+ ", 판매량: " + this.countSold
+				+ ", 재고량: " + this.countStock);
+	}
+
 }
--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -6,17 +6,18 @@ public class GoodsApp {
 		// 기본 생성
 		Goods goods = new Goods();
 
-		goods.name = "nikon";
-		goods.price = 400000;
-		goods.countSold = 10;
-		goods.countStock = 20;
-
-		System.out.println(
-				"상품 이름: " + goods.name 
-				+ ", 가격: " + goods.price 
-				+ ", 판매량: " + goods.countSold 
-				+ ", 재고량: " + goods.countStock);
-
+		goods.setName("nikon");
+		goods.setPrice(400000);
+		goods.setCountSold(10);
+		goods.setCountStock(20);
+		
+//		System.out.println(
+//				"상품 이름: " + goods.name 
+//				+ ", 가격: " + goods.price 
+//				+ ", 판매량: " + goods.countSold 
+//				+ ", 재고량: " + goods.countStock);
+		
+		goods.printInfo();
 	}
 
 }
