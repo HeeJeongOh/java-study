ac36a0af82d19fb8d14f967eaa90dbdcaf2edec6
HeeJeongOh <ohhj1999@gmail.com>
2024-11-22 13:27:40 +0900

    instanceof 연산

--- chapter03/src/main/java/paint/PaintApp.java
+++ chapter03/src/main/java/paint/PaintApp.java
@@ -3,24 +3,23 @@ package paint;
 public class PaintApp {
 	public static void main(String[] args) {
 		Point point1 = new Point();
-		
+
 		point1.setX(10);
 		point1.setY(20);
-		
+
 //		point1.show();
 //		drawPoint(point1);
 		draw(point1);
-		
+
 		Point point2 = new Point(100, 200);
-		
+
 //		point2.show(true);
 //		drawPoint(point2);
 		draw(point2);
-		
+
 //		point2.disappear();
 		point2.show(false);
-		
-		
+
 //		ColorPoint point3 = new ColorPoint();
 //		
 //		point3.setX(50);
@@ -28,40 +27,60 @@ public class PaintApp {
 //		point3.setColor("red");
 //
 //		drawPoint(point3);
-		
-		
+
 		ColorPoint point4 = new ColorPoint(50, 100, "yellow");
 //		point4.show();
 		draw(point4);
-		
 
 //		drawTriangle(new Triangle());
 //		drawRectangle(new Rectangle());
-		
+
 //		drawShape(new Triangle());
 //		drawShape(new Rectangle());
 //		drawShape(new Circle());
-		
-		
+
 		draw(new Triangle());
 		draw(new Rectangle());
 		draw(new Circle());
-		
+
 		draw(new GraphicText("안녕"));
+
+		// instanceof
+		Circle c = new Circle();
+
+		System.out.println(c instanceof Circle);
+		System.out.println(c instanceof Shape);
+		System.out.println(c instanceof Drawable);
+
+		// 오류 - 계층 관계가 존재해야 사용 가능
+		// Object - Drawable, Shape - Circle
+//		System.out.println(c instanceof Rectangle);
+		
+
+		Shape s = new Circle();
+
+		System.out.println(s instanceof Circle);
+		System.out.println(s instanceof Shape);
+		System.out.println(s instanceof Drawable);
+		System.out.println(s instanceof Rectangle);
+
+		System.out.println(c instanceof Drawable);
+		System.out.println(c instanceof Runnable);
+
 	}
-	
+
 	public static void draw(Drawable drawable) {
 		drawable.draw();
 	}
-	
+
 //	public static void drawPoint(Point point) {
 //		point.show();
 //	}	
-	
+
 //	public static void drawColorPoint(ColorPoint colorPoint) {
 //		colorPoint.show();
 //	}
-	
+
 //	public static void drawShape(Shape shape) {
 //		shape.draw();
 //	}
