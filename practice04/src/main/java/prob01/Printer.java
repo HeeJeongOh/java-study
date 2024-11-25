5b0f881f84dbb799bf5cfc92934820fb4cf80e1e
HeeJeongOh <ohhj1999@gmail.com>
2024-11-25 16:02:19 +0900

    Generic
    제네릭 클래스, 제네릭 메소드, 가변 파라미터

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,20 +2,40 @@ package prob01;
 
 public class Printer {
 
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String string) {
+//		System.out.println(string);
+//	}
+
+	// 제네릭 메소드
+	public <T> void println(T type) {
+		System.out.println(type);
 	}
 
-	public void println(boolean b) {
-		System.out.println(b);
+	// 가변 파라미터
+	public int sum(Integer... nums) {
+		int s = 0;
+		for (Integer n : nums) {
+			s += n;
+		}
+		return s;
 	}
 
-	public void println(double d) {
-		System.out.println(d);
+	public <T> void println(T... ts) {
+		for (T t : ts) {
+			System.out.print(t + " ");
+		}
+		System.out.println();
 	}
-
-	public void println(String string) {
-		System.out.println(string);
-	}
-
 }
