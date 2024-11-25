8de913e9ab502e18ca5eb503a6ee9868c7d3d982
HeeJeongOh <ohhj1999@gmail.com>
2024-11-25 13:00:53 +0900

    String Class

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,18 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+
+		// \t : tab
+		// \r : carriage return
+		// \n : newline
+
+		// "hello"
+		System.out.println("\"hello\"");
+
+	}
+
+}
---
+++ chapter04/src/main/java/chapter04/StringTest02.java
@@ -0,0 +1,35 @@
+package chapter04;
+
+public class StringTest02 {
+
+	public static void main(String[] args) {
+		// immutability(불변성)
+		String s1 = "abc";
+		String s2 = "def";
+		String s3 = s2;
+
+		// 새로운 객체를 생성하여 적용
+		s2 = s1.toUpperCase();
+
+		String s4 = s2.concat("??");
+		String s5 = "!".concat(s2).concat("@");
+
+		System.out.println("s1 : " + s1);
+		System.out.println("s2 : " + s2);
+		System.out.println("s3 : " + s3);
+		System.out.println("s4 : " + s4);
+		System.out.println("s5 : " + s5);
+
+		System.out.println(equalsHello("hello"));
+		System.out.println(equalsHello(null));
+	}
+
+	private static boolean equalsHello(String s) {
+		// null point exception 발생 가능
+		// return s.equals("hello");
+
+		// 피하기 가능
+		return "hello".equals(s);
+	}
+
+}
